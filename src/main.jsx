06542e4d83997f8d995f1a1f081439d2d3636cb3
Alessandro Fiumicelli <afiumicelli@MBP-afiumicelli.local>
2026-04-01 11:38:22 +0200

    add speedinsights

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { inject } from '@vercel/analytics'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 inject()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
